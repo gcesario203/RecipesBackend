9891ed0e76870df6fbaea9b6d6d0ae1d1bfff5f6
Gabriel Cesario <cesario203@outlook.com>
2020-10-10 09:31:33 -0300

    Removendo dependencias do postgres

--- api/categorias/controllers/categoryController.js
+++ api/categorias/controllers/categoryController.js
@@ -1,9 +1,9 @@
-module.exports = app =>{
-    const saveCategory = async(req,res)=>{
-        const user = {...req.body}
+// module.exports = app =>{
+//     const saveCategory = async(req,res)=>{
+//         const user = {...req.body}
 
-        if(user.admin !== true){
-            res.s
-        }
-    }
-}
+//         if(user.admin !== true){
+//             res.s
+//         }
+//     }
+// }
