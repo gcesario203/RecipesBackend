50b9e5be665cdd62ea0d2a06b11a64c722df7a89
Gabriel Cesario <cesario203@outlook.com>
2020-10-10 18:47:51 -0300

    Finalizando transaction de update de receitas

--- api/recipes/controllers/recipesController.js
+++ api/recipes/controllers/recipesController.js
@@ -1,106 +1,130 @@
 const { transaction } = require("../../../config/db")
 
-module.exports = app =>{
-    const {notExistOrError,existOrError,countItensInCollections,validId,checkIfItemExists} = app.api.global
+module.exports = app => {
+    const { notExistOrError, existOrError, countItensInCollections, validId, checkIfItemExists } = app.api.global
 
-    const save = async(req,res)=>{
-        const recipe = {...req.body.receita}
+    const save = async (req, res) => {
+        const recipe = { ...req.body.receita }
         const ingredientes = [...req.body.ingredientes]
         const categoria = req.body.categoria_id
-        
-        if(req.params.id) recipe.receita_id = req.params.id
 
-        try{
-            existOrError(recipe.nome_receita,"Nome da receita não informado")
-            existOrError(recipe.modo_preparo,"Modo de preparo da receita não informado")
-            existOrError(recipe.local_origem,"Local de origam da receita não informado")
-            existOrError(recipe.tempo_preparo,"Tempo de preparo não informado receita não informado")
-            existOrError(recipe.usuario_id,"Criador da receita não informado")
+        if (req.params.id) recipe.receita_id = req.params.id
+
+        try {
+            existOrError(recipe.nome_receita, "Nome da receita não informado")
+            existOrError(recipe.modo_preparo, "Modo de preparo da receita não informado")
+            existOrError(recipe.local_origem, "Local de origam da receita não informado")
+            existOrError(recipe.tempo_preparo, "Tempo de preparo não informado receita não informado")
+            existOrError(recipe.usuario_id, "Criador da receita não informado")
             existOrError(ingredientes, "Receita precisa de ingredientes")
             existOrError(categoria, "A receita precisa de uma categoria")
-        }catch(msg){
+        } catch (msg) {
             return res.status(400).send(msg)
         }
 
         const categoriaDb = await app.db("categorias")
-                                    .where({categoria_id:categoria})
-                                    .first()
+            .where({ categoria_id: categoria })
+            .first()
 
-        for(let l in ingredientes){
-            console.log(ingredientes[l])
+        if (recipe.receita_id) {
+            app.db.transaction(async trx => {
+                await app.db('receitas')
+                    .transacting(trx)
+                    .where({receita_id:recipe.receita_id})
+                    .update(recipe)
+                    .then(async () => {
+                        await app.db("categorizar")
+                            .transacting(trx)
+                            .update({ categoria_id: categoriaDb.categoria_id, receita_id: recipe.receita_id})
+                            .where({receita_id:recipe.receita_id})
+                            .then(async () => {
+                                await app.db("ingredientes")
+                                        .del()
+                                        .where({receita_id:recipe.receita_id})
+                                        
+                                for (let lIngredient in ingredientes) {
+                                    await app.db("ingredientes")
+                                        .transacting(trx)
+                                        .insert({ ...ingredientes[lIngredient], receita_id: recipe.receita_id})
+                                        .then(() => { trx.commit; })
+                                        .catch(()=>trx.rollback)
+                                }
+                            })
+                            .then(trx.commit)
+                            .catch(trx.rollback);
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+            })
+                .then(() => res.status(200).send("Receita atualizada com sucesso"))
+                .catch(() => res.status(500).send("Falha ao atualizar receita"))
+        } else {
+            app.db.transaction(async trx => {
+                await app.db('receitas')
+                    .transacting(trx)
+                    .insert(recipe)
+                    .then(async receitaId => {
+                        await app.db("categorizar")
+                            .transacting(trx)
+                            .insert({ categoria_id: categoriaDb.categoria_id, receita_id: receitaId[0] })
+                            .then(async () => {
+                                for (let lIngredient in ingredientes) {
+                                    console.log("uiui")
+                                    await app.db("ingredientes")
+                                        .transacting(trx)
+                                        .insert({ ...ingredientes[lIngredient], receita_id: receitaId[0] })
+                                        .then(() => { trx.commit; })
+                                        .catch(trx.rollback)
+                                }
+                            })
+                            .then(trx.commit)
+                            .catch(trx.rollback);
+                    })
+                    .then(trx.commit)
+                    .catch(trx.rollback)
+            })
+                .then(() => res.status(200).send("Receita cadastrada com sucesso"))
+                .catch(() => res.status(500).send("Falha ao cadastrar receita"))
         }
-
-        app.db.transaction(async trx =>{
-                if(recipe.receita_id){
-
-                }else{
-
-                }
-                    await app.db('receitas')
-                         .transacting(trx)
-                         .insert(recipe)
-                         .then(async receitaId=>{
-                            console.log(receitaId[0])
-                            await app.db("categorizar")
-                                .transacting(trx)
-                                .insert({categoria_id:categoriaDb.categoria_id,receita_id:receitaId[0]})
-                                .then(async ()=>{
-                                    for(let lIngredient in ingredientes){
-                                        console.log("uiui")
-                                        await app.db("ingredientes")
-                                                 .transacting(trx)
-                                                 .insert({...ingredientes[lIngredient],receita_id:receitaId[0]})
-                                                 .then(()=>{trx.commit;})
-                                                 .catch(trx.rollback)
-                                    }
-                                })
-                                .then(trx.commit)
-                                .catch(trx.rollback);
-                         })
-                        .then(trx.commit)
-                        .catch(trx.rollback)
-        })
-        .then(() => res.status(200).send("Receita cadastrada com sucesso"))
-        .catch(() => res.status(500).send("Falha ao cadastrar receita"))
     }
 
     const limit = 3
 
-    const get = async(req,res) =>{
+    const get = async (req, res) => {
         const page = req.query.page || 1
 
-            const result = await countItensInCollections('receitas','receita_id')
-            const count = parseInt(result.count)
-    
+        const result = await countItensInCollections('receitas', 'receita_id')
+        const count = parseInt(result.count)
+
 
         await app.db('receitas')
-            .select('receita_id', 'nome_receita', 'modo_preparo','local_origem','tempo_preparo','usuario_id',)
+            .select('receita_id', 'nome_receita', 'modo_preparo', 'local_origem', 'tempo_preparo', 'usuario_id',)
             .limit(limit).offset(page * limit - limit)
             .then(receitas => res.json({ data: receitas, count, limit }))
             .catch(err => res.status(500).send(err))
     }
 
-    const getById = async(req,res)=>{
+    const getById = async (req, res) => {
         try {
             validId(req.params.id, "Id invalido")
 
-            const existId = await checkIfItemExists('receitas',req.params.id)
-            
-            existOrError(existId,'Receita inexistente')
+            const existId = await checkIfItemExists('receitas', req.params.id)
+
+            existOrError(existId, 'Receita inexistente')
         } catch (err) {
             return res.status(400).send(err)
         }
 
         await app.db('receitas')
-        .select('receita_id', 'nome_receita', 'modo_preparo','local_origem','tempo_preparo','usuario_id')
-            .where({receita_id: req.params.id})
-            .then(receita=> res.status(200).json({
-                data:receita
+            .select('receita_id', 'nome_receita', 'modo_preparo', 'local_origem', 'tempo_preparo', 'usuario_id')
+            .where({ receita_id: req.params.id })
+            .then(receita => res.status(200).json({
+                data: receita
             }))
             .catch(err => res.status(500).send(err))
     }
 
-    const remove = async (req,res) =>{
+    const remove = async (req, res) => {
         try {
             validId(req.params.id, "ID invalido")
             const rowsDeleted = await app.db('receitas')
@@ -119,7 +143,7 @@ module.exports = app =>{
         }
     }
 
-    return{
+    return {
         save,
         get,
         getById,
